8c6ff2c60d3c553788369064ed94393fe0e70e9c
Rafael <rafael@rafael-Dell-System-XPS-L502X.(none)>
2013-07-21 15:49:36 -0300

    Added led test 1

---
+++ led1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)
+count = 0
+while True:
+	mybutton = GPIO.input(11)
+	if mybutton == False:
+		count = count + 1
+		print "Yahoo!", count
+		time.sleep(.2)
