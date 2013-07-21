04c153e9ecc3d98eef463ac538bcadd6138512ab
Rafael <rafael@rafael-Dell-System-XPS-L502X.(none)>
2013-07-21 17:37:49 -0300

    led3.py

--- led3.py
+++ led3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+
+GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(12, GPIO.OUT)
@@ -10,3 +12,4 @@ GPIO.output(12, False)
 time.sleep(3)
 
 GPIO.output(12, True)
+GPIO.cleanup()
