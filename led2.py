52172ca86b82a7ea83469fe408f6fdce929c6319
Rafael <rafael@rafael-Dell-System-XPS-L502X.(none)>
2013-07-21 16:54:45 -0300

    Added second led script

---
+++ led2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+
+GPIO_BUTTON_INPUT = 11
+GPIO_BUTTON_OUTPUT = 12
+
+while True:
+	if GPIO.input(GPIO_BUTTON_INPUT):
+		GPIO.output(12, True)
+	else:
+		GPIO.output(12, False)
